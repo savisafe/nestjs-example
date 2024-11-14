7b0cff1ddf46d9b4823a84aff047662d4a4963b3
savisafe <savisafe@vk.com>
2024-11-15 01:22:42 +0500

    fix: cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import * as process from 'process';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: [process.env.CORS_DOMEN],
+    origin: true,
     credentials: true,
   });
   app.use(cookieParser());
