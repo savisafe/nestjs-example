6514f0057e40aed9c1b85a141661f69e53e5d2e7
Nick White <bbycinka@gmail.com>
2024-11-15 13:38:23 +0300

    fix

--- src/functions/setHead.ts
+++ src/functions/setHead.ts
@@ -1,6 +1,9 @@
 import * as process from 'process';
 
 export const setHead = (response) => {
-  response.setHeader('Access-Control-Allow-Origin', process.env.CORS_DOMEN);
+  response.setHeader(
+    'Access-Control-Allow-Origin',
+    `${process.env.CORS_DOMEN}`,
+  );
   response.setHeader('Access-Control-Allow-Credentials', 'true');
 };
