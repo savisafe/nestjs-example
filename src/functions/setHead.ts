7a81e9a5418f582f099368abee9d156c3eb5d44a
Nick White <bbycinka@gmail.com>
2024-11-15 15:29:16 +0300

    fix

--- src/functions/setHead.ts
+++ src/functions/setHead.ts
@@ -1,10 +1,11 @@
-import * as process from 'process';
+// import * as process from 'process';
 
 export const setHead = (response) => {
-  response.setHeader(
-    'Access-Control-Allow-Origin',
-    `${process.env.CORS_DOMEN}`,
-  );
-  response.setHeader('Access-Control-Allow-Origin', `http://localhost:3000`);
+  // response.setHeader(
+  //   'Access-Control-Allow-Origin',
+  //   `${process.env.CORS_DOMEN}`,
+  // );
+  // response.setHeader('Access-Control-Allow-Origin', `http://localhost:3000`);
+  response.setHeader('Access-Control-Allow-Origin', `*`);
   response.setHeader('Access-Control-Allow-Credentials', 'true');
 };
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
-import * as process from 'process';
+// import * as process from 'process';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: [`${process.env.CORS_DOMEN}`, 'http://localhost:3000'],
+    // origin: [`${process.env.CORS_DOMEN}`, 'http://localhost:3000'],
+    origin: true,
     credentials: true,
   });
   app.use(cookieParser());
