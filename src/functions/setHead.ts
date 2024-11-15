ff6251ba69aac4acbe4126f4a757c20fcfcf5c0f
Nick White <bbycinka@gmail.com>
2024-11-15 11:09:44 +0300

    fix

--- src/functions/setHead.ts
+++ src/functions/setHead.ts
@@ -3,7 +3,7 @@ import * as process from 'process';
 export const setHead = (response) => {
   response.setHeader(
     'Access-Control-Allow-Origin',
-    `${process.env.CORS_DOMEN}`,
+    process.env.CORS_DOMEN_PRODUCTION,
   );
   response.setHeader('Access-Control-Allow-Credentials', 'true');
 };
--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,7 @@ import * as process from 'process';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: [
-      `${process.env.CORS_DOMEN}`,
-      `${process.env.CORS_DOMEN_PRODUCTION}`,
-    ],
+    origin: [process.env.CORS_DOMEN_PRODUCTION],
     credentials: true,
   });
   app.use(cookieParser());
