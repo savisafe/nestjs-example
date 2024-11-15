d6375cc4a8411ddd03d23331b3238c5b266c7ca9
Nick White <bbycinka@gmail.com>
2024-11-15 14:17:55 +0300

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
-import * as process from 'process';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: [`${process.env.CORS_DOMEN}`, 'http://localhost:3000'],
+    origin: true,
     credentials: true,
   });
   app.use(cookieParser());
