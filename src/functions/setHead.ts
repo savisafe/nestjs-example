040b9ba37e34811d152823e025f007b83bae71ab
savisafe <savisafe@vk.com>
2024-12-12 23:18:39 +0500

    come back

--- src/functions/setHead.ts
+++ src/functions/setHead.ts
@@ -1,11 +1,6 @@
 import * as process from 'process';
 export const setHead = (response) => {
-  response.setHeader(
-    'Access-Control-Allow-Origin',
-    process.env.NODE_ENV === 'production'
-      ? 'https://whox.is'
-      : 'http://localhost:3000',
-  );
+  response.setHeader('Access-Control-Allow-Origin', process.env.CORS_DOMEN);
   response.setHeader('Access-Control-Allow-Credentials', 'true');
   response.setHeader(
     'Access-Control-Allow-Methods',
--- src/main.ts
+++ src/main.ts
@@ -6,21 +6,13 @@ import * as process from 'process';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin:
-      process.env.NODE_ENV === 'production'
-        ? ['https://whox.is']
-        : ['http://localhost:3000'],
+    origin: [process.env.CORS_DOMEN],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
 
   app.use(function (req, res, next) {
-    res.header(
-      'Access-Control-Allow-Origin',
-      process.env.NODE_ENV === 'production'
-        ? 'https://whox.is'
-        : 'http://localhost:3000',
-    );
+    res.header('Access-Control-Allow-Origin', process.env.CORS_DOMEN || '*');
     res.header(
       'Access-Control-Allow-Methods',
       'GET, PUT, POST, DELETE, OPTIONS',
