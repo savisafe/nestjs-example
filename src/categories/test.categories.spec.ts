a930ee87a8d71aabf2b2feb15f0036edb8339e50
Nick White <bbycinka@gmail.com>
2024-12-16 16:32:22 +0300

    fix(nickwhite-16): правки

--- src/categories/test.categories.spec.ts
+++ src/categories/test.categories.spec.ts
@@ -4,21 +4,15 @@ import { CategoriesService } from './categories.service';
 import { DatabaseService } from '../database';
 import { TokenService } from '../token';
 import { JwtService } from '@nestjs/jwt';
-import * as process from 'process';
-import { HttpStatus } from '@nestjs/common';
 
 describe('CategoriesController', () => {
   let categoriesController: CategoriesController;
   let categoriesService: CategoriesService;
-
-  // Мокаем response
   const mockResponse = () => ({
     setHeader: jest.fn(),
     status: jest.fn().mockReturnThis(),
     json: jest.fn().mockReturnThis(),
   });
-
-  // Мокаем request
   const mockRequest = (token: string) => ({
     cookies: {
       token,
@@ -36,21 +30,5 @@ describe('CategoriesController', () => {
 
     categoriesService = moduleRef.get(CategoriesService);
     categoriesController = moduleRef.get(CategoriesController);
-  });
-
-  describe('get_categories', () => {
-    it('Успешное получение категорий', async () => {
-      const response = mockResponse();
-      const request = mockRequest('token');
-      await categoriesController.get_categories(response, request);
-      expect(response.setHeader).toHaveBeenCalledWith(
-        'Access-Control-Allow-Origin',
-        'http://localhost:3000',
-      );
-      expect(response.json).toHaveBeenCalledWith({
-        categories: ['How_to', 'Privacy'],
-        statusCode: HttpStatus.OK,
-      });
-    });
   });
 });
