ca832e32193716e4be8d2304a736cdb8fdb3d9fa
Nick White <bbycinka@gmail.com>
2025-01-16 15:42:02 +0300

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -3,24 +3,7 @@ import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const whitelist = ['https://whox.is', 'http://localhost:3000'];
-  app.enableCors({
-    origin: function (origin, callback) {
-      if (whitelist.indexOf(origin) !== -1) {
-        console.log('allowed cors for:', origin);
-        callback(null, true);
-      } else {
-        console.log('blocked cors for:', origin);
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    allowedHeaders:
-      'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
-    methods: 'GET,PUT,POST,DELETE,UPDATE,OPTIONS',
-    credentials: true,
-  });
-
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.use(cookieParser());
   app.setGlobalPrefix('api');
   await app.listen(3002);
