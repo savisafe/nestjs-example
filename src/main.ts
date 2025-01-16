8828b891ce39ac23a60f3a25a14aa931480c63c2
nickwhite <bbycinka@gmail.com>
2025-01-16 10:31:21 +0000

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import * as process from 'process';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: [process.env.CORS_DOMEN_LOCAL],
+    origin: [process.env.CORS_DOMEN],
     credentials: true,
   });
 
