c813218d21dd5b3525454f8a68cc22fe22f22464
Nick White <bbycinka@gmail.com>
2025-01-17 08:46:47 +0300

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
-import * as process from 'process';
+// import * as process from 'process';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: [process.env.CORS_DOMEN],
-    credentials: true,
-  });
   app.use(cookieParser());
   app.setGlobalPrefix('api');
   await app.listen(3002);
